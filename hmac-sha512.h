ebccb80556a475e36d639dd5ca59880b8c0486b6
Mike Tubby <mike@orac.tubby.org>
2025-11-03 12:13:28 +0000

    Fix copyright/license on sha512/hmac-sha512 modules

--- hmac-sha512.h
+++ hmac-sha512.h
@@ -2,7 +2,17 @@
  * hmac_sha512.h - simple HMAC-SHA512 implementation (C11, procedural)
  *
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025
+ * Copyright (C) 2025 Mike Tubby G8TIC mike@tubby.org and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ *
  */
 
 #ifndef _HMAC_SHA512_H
--- sha512.h
+++ sha512.h
@@ -2,7 +2,7 @@
  * sha512.h - simple, portable SHA-512 implementation (C11, procedural)
  *
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025
+ * Copyright (C) 2025 Mike Tubby G8TIC mike@tubby.org and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -12,6 +12,7 @@
  * furnished to do so, subject to the following conditions:
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ *
  */
 
 #ifndef _SHA512_H
